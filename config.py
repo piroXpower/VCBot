14edfe1942646e5b3b0959baad8ac027c4d77fca
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-07 01:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
 
 # Please edit 'CONFIG = False' to 'CONFIG = True'
-CONFIG = True
+CONFIG = False
 
 # Add your APP ID from my.telegram.org
 API_ID = 3898418
