e0768802a277f294a20ccbe41ccfcbae0db312ec
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-06 15:26:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+
+# Please edit 'CONFIG = False' to 'CONFIG = True'
+CONFIG = True
+
+# Add your APP ID from my.telegram.org
+API_ID = 3898418
+
+# Add your API HASH from my.telegram.org
+API_HASH = "5a82313211da04d63297bd4de436228c"
+
+# Read readme and generate string session. Then add that to here.
+SESSION = ""
+
+# Add your bot token here
+TOKEN = ""
+
+# Chat id where bot should work.
+CHAT_ID = -1001631657857
+
+# Add ur TG ID, Seperate by spaces
+ADMINS = 5185988169
